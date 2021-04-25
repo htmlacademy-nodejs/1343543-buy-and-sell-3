79197a7e2a155749180880cddaaa2dc8a8031010
Dmitry <dmitry.khalimov@gmail.com>
2021-04-25 18:43:46 +0300

    Переводит writefile на рельсы async/await

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -77,7 +77,7 @@ const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.
 
 const generateOffers = (count) => {
   if (count > MocksCount.MAX) {
-    console.error(`Не больше 1000 объявлений`);
+    console.error(chalk.red(`Не больше 1000 объявлений`));
     process.exit(ExitCode.error);
   }
 
@@ -93,19 +93,18 @@ const generateOffers = (count) => {
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || MocksCount.DEFAULT;
     const content = JSON.stringify(generateOffers(countOffer));
 
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Can't write data to file...`));
-      }
-
-      return console.info(chalk.green(`Operation success. File created.`));
-    });
+    try {
+      await fs.writeFile(FILE_NAME, content, () => {});
+      console.log(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.log(err);
+      console.error(chalk.red(`Can't write data to file...`));
+    }
   }
 };
-
