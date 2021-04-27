28704ad27815616e8bccbcbefabae84ab87e8e74
Dmitry <dmitry.khalimov@gmail.com>
2021-04-27 23:48:26 +0300

    Заканчивает домашнее задание

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Подключаем модуль `fs`
-const fs = require(`fs`);
+const fs = require(`fs/promises`);
 const chalk = require(`chalk`);
 
 const {
@@ -15,43 +15,9 @@ const {
 
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-  `Продам коллекцию журналов «Огонёк».`,
-  `Отдам в хорошие руки подшивку «Мурзилка».`,
-  `Продам советскую посуду. Почти не разбита.`,
-  `Куплю детские санки.`
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  `Кажется, что это хрупкая вещь.`,
-  `Мой дед не мог её сломать.`,
-  `Кому нужен этот новый телефон, если тут такое...`,
-  `Не пытайтесь торговаться. Цену вещам я знаю.`
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const OfferType = {
   OFFER: `offer`,
@@ -75,17 +41,18 @@ const PictureRestrict = {
 
 const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.jpg`;
 
-const generateOffers = (count) => {
+const generateOffers = (params) => {
+  const {count, titles, categories, sentences} = params;
   if (count > MocksCount.MAX) {
     console.error(chalk.red(`Не больше 1000 объявлений`));
     process.exit(ExitCode.error);
   }
 
   return Array(count).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
   }));
@@ -94,10 +61,23 @@ const generateOffers = (count) => {
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const readContent = async (filePath) => {
+      try {
+        const content = await fs.readFile(filePath, `utf8`);
+        return content.split(`\n`);
+      } catch (err) {
+        console.error(chalk.red(err));
+        return [];
+      }
+    };
+
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || MocksCount.DEFAULT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers({count: countOffer, sentences, titles, categories}));
 
     try {
       await fs.writeFile(FILE_NAME, content, () => {});
