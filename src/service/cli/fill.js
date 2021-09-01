9375d13d4c5469a1e7076af80454a91103bc5b1c
Dmitry <dmitry.khalimov@gmail.com>
2021-09-01 23:25:23 +0300

    Чинит fill

--- src/service/cli/fill.js
+++ src/service/cli/fill.js
@@ -1,24 +1,35 @@
 'use strict';
 
+// Подключаем модуль `fs`
+const fs = require(`fs/promises`);
 const chalk = require(`chalk`);
-const fs = require(`fs`).promises;
+
 const {
   getRandomInt,
   shuffle,
 } = require(`../../utils`);
 
-const DEFAULT_COUNT = 1;
-const MAX_COMMENTS = 4;
+const {
+  ExitCode
+} = require(`../../constants`);
 
-const FILE_NAME = `fill-db.sql`;
+const FILE_NAME = `fill-db2.sql`;
+
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 const FILE_COMMENTS_PATH = `./data/comments.txt`;
 
+const MAX_COMMENTS = 4;
+
 const OfferType = {
   OFFER: `offer`,
   SALE: `sale`,
+};
+
+const MocksCount = {
+  DEFAULT: 1,
+  MAX: 1000
 };
 
 const SumRestrict = {
@@ -31,15 +42,7 @@ const PictureRestrict = {
   MAX: 16,
 };
 
-const readContent = async (filePath) => {
-  try {
-    const content = await fs.readFile(filePath, `utf8`);
-    return content.trim().split(`\n`);
-  } catch (err) {
-    console.error(chalk.red(err));
-    return [];
-  }
-};
+const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.jpg`;
 
 const generateComments = (count, offerId, userCount, comments) => (
   Array(count).fill({}).map(() => ({
@@ -51,51 +54,67 @@ const generateComments = (count, offerId, userCount, comments) => (
   }))
 );
 
-const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.jpg`;
+const generateOffers = (params) => {
+  const {count, commentSentences, titles, categoryCount, userCount, sentences} = params;
+  if (count > MocksCount.MAX) {
+    console.error(chalk.red(`Не больше 1000 объявлений`));
+    process.exit(ExitCode.error);
+  }
 
-const generateOffers = (count, titles, categoryCount, userCount, sentences, comments) => (
-  Array(count).fill({}).map((_, index) => ({
+  return Array(count).fill({}).map((_, index) => ({
     category: [getRandomInt(1, categoryCount)],
-    comments: generateComments(getRandomInt(1, MAX_COMMENTS), index + 1, userCount, comments),
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), index + 1, userCount, commentSentences),
     description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
     title: titles[getRandomInt(0, titles.length - 1)],
-    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
+    type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
     userId: getRandomInt(1, userCount)
-  }))
-);
+  }));
+};
+
+const users = [
+  {
+    email: `ivanov@example.com`,
+    passwordHash: `5f4dcc3b5aa765d61d8327deb882cf99`,
+    firstName: `Иван`,
+    lastName: `Иванов`,
+    avatar: `avatar1.jpg`
+  },
+  {
+    email: `petrov@example.com`,
+    passwordHash: `5f4dcc3b5aa765d61d8327deb882cf99`,
+    firstName: `Пётр`,
+    lastName: `Петров`,
+    avatar: `avatar2.jpg`
+  }
+];
 
 module.exports = {
   name: `--fill`,
   async run(args) {
-    const sentences = await readContent(FILE_SENTENCES_PATH);
-    const titles = await readContent(FILE_TITLES_PATH);
-    const categories = await readContent(FILE_CATEGORIES_PATH);
-    const commentSentences = await readContent(FILE_COMMENTS_PATH);
+    const readContent = async (filePath) => {
+      try {
+        const content = await fs.readFile(filePath, `utf8`);
+        return content.split(`\n`);
+      } catch (err) {
+        console.error(chalk.red(err));
+        return [];
+      }
+    };
+
+    const [commentSentences, sentences, titles, categories] = await Promise.all([
+      readContent(FILE_COMMENTS_PATH),
+      readContent(FILE_SENTENCES_PATH),
+      readContent(FILE_TITLES_PATH),
+      readContent(FILE_CATEGORIES_PATH),
+    ]);
+
+    console.log(commentSentences);
 
     const [count] = args;
-    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-
-    const users = [
-      {
-        email: `ivanov@example.com`,
-        passwordHash: `5f4dcc3b5aa765d61d8327deb882cf99`,
-        firstName: `Иван`,
-        lastName: `Иванов`,
-        avatar: `avatar1.jpg`
-      },
-      {
-        email: `petrov@example.com`,
-        passwordHash: `5f4dcc3b5aa765d61d8327deb882cf99`,
-        firstName: `Пётр`,
-        lastName: `Петров`,
-        avatar: `avatar2.jpg`
-      }
-    ];
-
-
-    const offers = generateOffers(countOffer, titles, categories.length, users.length, sentences, commentSentences);
+    const countOffer = Number.parseInt(count, 10) || MocksCount.DEFAULT;
+    const offers = generateOffers({count: countOffer, sentences, titles, categories: categories.length, commentSentences, users: users.length});
 
     const comments = offers.flatMap((offer) => offer.comments);
 
@@ -141,11 +160,11 @@ INSERT INTO COMMENTS(text, user_id, offer_id) VALUES
 ${commentValues};
 ALTER TABLE comments ENABLE TRIGGER ALL;`;
 
-
     try {
       await fs.writeFile(FILE_NAME, content);
       console.log(chalk.green(`Operation success. File created.`));
     } catch (err) {
+      console.log(err);
       console.error(chalk.red(`Can't write data to file...`));
     }
   }
